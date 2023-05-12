2fb9c1f8b82c78b9438da4889d58daf895705495
HarrisonZussman <115054515+HarrisonZussman@users.noreply.github.com>
2023-05-11 21:41:19 -0400

    log back in

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -8,10 +8,10 @@ const {
 // Get all users
 router.get('/', (req, res) => {
     User.findAll({
-            attributes: {
-                exclude: ['password']
-            }
-        })
+        attributes: {
+            exclude: ['password']
+        }
+    })
         .then(databaseUserData => res.json(databaseUserData))
         .catch(err => {
             console.log(err);
@@ -22,26 +22,26 @@ router.get('/', (req, res) => {
 // Get specific user
 router.get('/:id', (req, res) => {
     User.findOne({
-            attributes: {
-                exclude: ['password']
-            },
-            where: {
-                id: req.params.id
-            },
-            include: [{
-                    model: Post,
-                    attributes: ['id', 'title', 'content', 'created_at']
-                },
-                {
-                    model: Comment,
-                    attributes: ['id', 'comment_text', 'created_at'],
-                    include: {
-                        model: Post,
-                        attributes: ['title']
-                    }
-                }
-            ]
-        })
+        attributes: {
+            exclude: ['password']
+        },
+        where: {
+            id: req.params.id
+        },
+        include: [{
+            model: Post,
+            attributes: ['id', 'title', 'content', 'created_at']
+        },
+        {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'created_at'],
+            include: {
+                model: Post,
+                attributes: ['title']
+            }
+        }
+        ]
+    })
         .then(databaseUserData => {
             if (!databaseUserData) {
                 res.status(404).json({
@@ -60,9 +60,9 @@ router.get('/:id', (req, res) => {
 // Create a user
 router.post('/', (req, res) => {
     User.create({
-            username: req.body.username,
-            password: req.body.password
-        })
+        username: req.body.username,
+        password: req.body.password
+    })
         .then(databaseUserData => {
             req.session.save(() => {
                 req.session.user_id = databaseUserData.id;
@@ -80,12 +80,12 @@ router.post('/', (req, res) => {
 
 router.post('/login', (req, res) => {
     User.findOne({
-            where: {
-                username: req.body.username
-            }
-        })
+        where: {
+            username: req.body.username
+        }
+    })
         .then(databaseUserData => {
-
+            console.log(databaseUserData.username)
             if (!databaseUserData) {
                 res.status(400).json({
                     message: 'No user with that username!'
@@ -93,17 +93,19 @@ router.post('/login', (req, res) => {
                 return;
             }
 
-
+            console.log(databaseUserData)
             req.session.save(() => {
+                console.log(3)
                 req.session.user_id = databaseUserData.id;
                 req.session.username = databaseUserData.username;
                 req.session.loggedIn = true;
-
+                console.log(4)
                 res.json({
                     user: databaseUserData,
                     message: 'You are now logged in!'
                 });
             });
+            console.log(1)
 
             const validPassword = databaseUserData.checkPassword(req.body.password);
 
@@ -114,16 +116,11 @@ router.post('/login', (req, res) => {
                 return;
             }
 
-            req.session.save(() => {
-                req.session.user_id = databaseUserData.id;
-                req.session.username = databaseUserData.username;
-                req.session.loggedIn = true;
 
-                res.json({
-                    user: databaseUserData,
-                    message: 'You are now logged in!'
-                });
-            });
+            console.log(2)
+        }).catch(err => {
+            console.log(err);
+            res.status(500).json(err);
         });
 });
 
